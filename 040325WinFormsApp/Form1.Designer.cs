f6c8c19049ee26f553e679807df46e6523dcd868
Sanx <martinezsantiago1607@gmail.com>
2025-03-04 19:56:01 -0600

    Add project files.

---
+++ 040325WinFormsApp/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using _040325WinFormsApp;
+
+namespace _250225
+{
+    public partial class Form1 : Form
+    {
+        private DiscoRepository repo;
+
+        public Form1()
+        {
+            InitializeComponent();
+            repo = new DiscoRepository();
+            CargarDiscos();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            var disco = new Disco
+            {
+                nombre = txtNombre.Text,
+                artista = txtArtista.Text,
+                genero = txtGenero.Text,
+                anio = int.Parse(txtAnio.Text),
+                precio = decimal.Parse(txtPrecio.Text)
+            };
+
+            repo.InsertarDisco(disco);
+            CargarDiscos();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (dgvDiscos.SelectedRows.Count > 0)
+            {
+                int id = Convert.ToInt32(dgvDiscos.SelectedRows[0].Cells[0].Value);
+                repo.EliminarDisco(id);
+                CargarDiscos();
+            }
+            else
+            {
+                MessageBox.Show("Selecciona un disco para eliminar.");
+            }
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            if (dgvDiscos.SelectedRows.Count > 0)
+            {
+                var disco = new Disco
+                {
+                    id = Convert.ToInt32(dgvDiscos.SelectedRows[0].Cells[0].Value),
+                    nombre = txtNombre.Text,
+                    artista = txtArtista.Text,
+                    genero = txtGenero.Text,
+                    anio = int.Parse(txtAnio.Text),
+                    precio = decimal.Parse(txtPrecio.Text)
+                };
+
+                repo.ActualizarDisco(disco);
+                CargarDiscos();
+            }
+            else
+            {
+                MessageBox.Show("Selecciona un disco para actualizar.");
+            }
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            CargarDiscos();
+        }
+
+        private void CargarDiscos()
+        {
+            List<Disco> discos = repo.ObtenerDisco();
+            dgvDiscos.DataSource = discos;
+        }
+    }
+}
---
+++ 040325WinFormsApp/IDiscoRepository.cs
@@ -0,0 +1,19 @@
+﻿using _040325WinFormsApp;
+using _250225;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _040325WinFormsApp
+{
+    public interface IDiscoRepository
+    {
+        void InsertarDisco(Disco disco);
+        void ActualizarDisco(Disco disco);
+        void EliminarDisco(int id); // Cambiar aquí para aceptar un int
+        List<Disco> ObtenerDisco();
+    }
+
+}
