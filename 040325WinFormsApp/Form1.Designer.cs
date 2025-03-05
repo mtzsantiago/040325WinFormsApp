5c648e4b350600207750073ecb3ed8988bafda9c
Sanx <martinezsantiago1607@gmail.com>
2025-03-04 20:06:36 -0600

    cambios

--- 040325WinFormsApp/Form1.Designer.cs
+++ 040325WinFormsApp/Form1.Designer.cs
@@ -1,82 +1,162 @@
-﻿using System;
-using System.Windows.Forms;
-using System.Collections.Generic;
-using _040325WinFormsApp;
-
-namespace _250225
+﻿namespace _040325WinFormsApp
 {
-    public partial class Form1 : Form
+    partial class Form1
     {
-        private DiscoRepository repo;
+        private System.ComponentModel.IContainer components = null;
+        private Button btnAgregar;
+        private Button btnEliminar;
+        private Button btnActualizar;
+        private Button btnListar;
+        private TextBox txtNombre;
+        private TextBox txtArtista;
+        private TextBox txtGenero;
+        private TextBox txtAnio;
+        private TextBox txtPrecio;
+        private DataGridView dgvDiscos;
 
-        public Form1()
+        // Método necesario para la limpieza de los recursos.
+        protected override void Dispose(bool disposing)
         {
-            InitializeComponent();
-            repo = new DiscoRepository();
-            CargarDiscos();
-        }
-
-        private void btnAgregar_Click(object sender, EventArgs e)
-        {
-            var disco = new Disco
+            if (disposing && (components != null))
             {
-                nombre = txtNombre.Text,
-                artista = txtArtista.Text,
-                genero = txtGenero.Text,
-                anio = int.Parse(txtAnio.Text),
-                precio = decimal.Parse(txtPrecio.Text)
-            };
-
-            repo.InsertarDisco(disco);
-            CargarDiscos();
-        }
-
-        private void btnEliminar_Click(object sender, EventArgs e)
-        {
-            if (dgvDiscos.SelectedRows.Count > 0)
-            {
-                int id = Convert.ToInt32(dgvDiscos.SelectedRows[0].Cells[0].Value);
-                repo.EliminarDisco(id);
-                CargarDiscos();
+                components.Dispose();
             }
-            else
-            {
-                MessageBox.Show("Selecciona un disco para eliminar.");
-            }
+            base.Dispose(disposing);
         }
 
-        private void btnActualizar_Click(object sender, EventArgs e)
+        // Inicialización de los componentes.
+        private void InitializeComponent()
         {
-            if (dgvDiscos.SelectedRows.Count > 0)
-            {
-                var disco = new Disco
-                {
-                    id = Convert.ToInt32(dgvDiscos.SelectedRows[0].Cells[0].Value),
-                    nombre = txtNombre.Text,
-                    artista = txtArtista.Text,
-                    genero = txtGenero.Text,
-                    anio = int.Parse(txtAnio.Text),
-                    precio = decimal.Parse(txtPrecio.Text)
-                };
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtArtista = new System.Windows.Forms.TextBox();
+            this.txtGenero = new System.Windows.Forms.TextBox();
+            this.txtAnio = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.dgvDiscos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDiscos)).BeginInit();
+            this.SuspendLayout();
 
-                repo.ActualizarDisco(disco);
-                CargarDiscos();
-            }
-            else
-            {
-                MessageBox.Show("Selecciona un disco para actualizar.");
-            }
-        }
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(12, 150);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(100, 23);
+            this.btnAgregar.TabIndex = 0;
+            this.btnAgregar.Text = "Agregar Disco";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
 
-        private void btnListar_Click(object sender, EventArgs e)
-        {
-            CargarDiscos();
-        }
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(118, 150);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(100, 23);
+            this.btnEliminar.TabIndex = 1;
+            this.btnEliminar.Text = "Eliminar Disco";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
 
-        private void CargarDiscos()
-        {
-            List<Disco> discos = repo.ObtenerDisco();
-            dgvDiscos.DataSource = discos;
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(224, 150);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(100, 23);
+            this.btnActualizar.TabIndex = 2;
+            this.btnActualizar.Text = "Actualizar Disco";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(330, 150);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(100, 23);
+            this.btnListar.TabIndex = 3;
+            this.btnListar.Text = "Listar Discos";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(12, 12);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 4;
+            this.txtNombre.PlaceholderText = "Nombre";
+
+            // 
+            // txtArtista
+            // 
+            this.txtArtista.Location = new System.Drawing.Point(118, 12);
+            this.txtArtista.Name = "txtArtista";
+            this.txtArtista.Size = new System.Drawing.Size(100, 20);
+            this.txtArtista.TabIndex = 5;
+            this.txtArtista.PlaceholderText = "Artista";
+
+            // 
+            // txtGenero
+            // 
+            this.txtGenero.Location = new System.Drawing.Point(224, 12);
+            this.txtGenero.Name = "txtGenero";
+            this.txtGenero.Size = new System.Drawing.Size(100, 20);
+            this.txtGenero.TabIndex = 6;
+            this.txtGenero.PlaceholderText = "Género";
+
+            // 
+            // txtAnio
+            // 
+            this.txtAnio.Location = new System.Drawing.Point(330, 12);
+            this.txtAnio.Name = "txtAnio";
+            this.txtAnio.Size = new System.Drawing.Size(100, 20);
+            this.txtAnio.TabIndex = 7;
+            this.txtAnio.PlaceholderText = "Año";
+
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(436, 12);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.TabIndex = 8;
+            this.txtPrecio.PlaceholderText = "Precio";
+
+            // 
+            // dgvDiscos
+            // 
+            this.dgvDiscos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDiscos.Location = new System.Drawing.Point(12, 179);
+            this.dgvDiscos.Name = "dgvDiscos";
+            this.dgvDiscos.Size = new System.Drawing.Size(524, 150);
+            this.dgvDiscos.TabIndex = 9;
+
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(584, 341);
+            this.Controls.Add(this.dgvDiscos);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtAnio);
+            this.Controls.Add(this.txtGenero);
+            this.Controls.Add(this.txtArtista);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Name = "Form1";
+            this.Text = "Gestión de Discos";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDiscos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
     }
 }
--- 040325WinFormsApp/Form1.cs
+++ 040325WinFormsApp/Form1.cs
@@ -1,10 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using _040325WinFormsApp;
+
 namespace _040325WinFormsApp
 {
     public partial class Form1 : Form
     {
+        private DiscoRepository repo;
+
         public Form1()
         {
             InitializeComponent();
+            repo = new DiscoRepository();
+            CargarDiscos();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            var disco = new Disco
+            {
+                nombre = txtNombre.Text,
+                artista = txtArtista.Text,
+                genero = txtGenero.Text,
+                anio = int.Parse(txtAnio.Text),
+                precio = decimal.Parse(txtPrecio.Text)
+            };
+
+            repo.InsertarDisco(disco);
+            CargarDiscos();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (dgvDiscos.SelectedRows.Count > 0)
+            {
+                int id = Convert.ToInt32(dgvDiscos.SelectedRows[0].Cells[0].Value);
+                repo.EliminarDisco(id);
+                CargarDiscos();
+            }
+            else
+            {
+                MessageBox.Show("Selecciona un disco para eliminar.");
+            }
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            if (dgvDiscos.SelectedRows.Count > 0)
+            {
+                var disco = new Disco
+                {
+                    id = Convert.ToInt32(dgvDiscos.SelectedRows[0].Cells[0].Value),
+                    nombre = txtNombre.Text,
+                    artista = txtArtista.Text,
+                    genero = txtGenero.Text,
+                    anio = int.Parse(txtAnio.Text),
+                    precio = decimal.Parse(txtPrecio.Text)
+                };
+
+                repo.ActualizarDisco(disco);
+                CargarDiscos();
+            }
+            else
+            {
+                MessageBox.Show("Selecciona un disco para actualizar.");
+            }
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            CargarDiscos();
+        }
+
+        private void CargarDiscos()
+        {
+            List<Disco> discos = repo.ObtenerDisco();
+            dgvDiscos.DataSource = discos;
         }
     }
 }
